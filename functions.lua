c854e24918bb449d0ab6e8c43e44c084afc30ad9
Yan <121140658+yanlvl99@users.noreply.github.com>
2024-11-14 20:16:52 -0300

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -1,1 +1,14 @@
+function GetCurrentSea()
+    local hasLocation = LocationsFolder.FindFirstChild
+    if hasLocation("Mansion") and not hasLocation("Café") then
+        return "Third Sea"
+    elseif hasLocation("Café") then
+        return "Second Sea"
+    else
+        return "First Sea"
+    end
+end
 
+function AddStatPoints(Type,Quantity)
+    CommF_:InvokeServer("AddPoint",Type or "Melee",Quantity or 1)
+end
